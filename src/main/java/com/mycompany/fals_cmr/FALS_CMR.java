5ab19fc986603fb0588da1931260b4212c3128d7
Carvajal14 <115636793+Carvajal14@users.noreply.github.com>
2023-08-16 13:47:32 -0500

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/fals_cmr/FALS_CMR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fals_cmr;
+
+/**
+ *
+ * @author santi_
+ */
+public class FALS_CMR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
